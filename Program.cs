193cbac70b86cde8fad3f6701fff90df3c7d0ed5
Yamanaeva Rimma <m.e.dragon@mail.ru>
2023-02-23 00:33:52 +0300

    задел

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AlgorithmComplexityTheory
 {
 	class Program
 	{
+		/// <summary>
+		/// Вариант 13. A = {w: w содержит ровно три 0 или ровно три 1}
+		/// </summary>
+		static string HW1(string input)
+		{
+			List<int> states = new List<int>() { 0 };//последовательность состояний детерминированного конечного автомата
+			HashSet<int> Q = new HashSet<int>() { 0, 1, 2 };//состояния
+			HashSet<int> Sigma = new HashSet<int>() { 0, 1 };//алфавит
+			for (int i = 0; i < input.Length; i++)
+			{
+
+			}
+			return string.Join(" ", states);
+		}
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string s = Console.ReadLine();
+			Console.WriteLine(HW1(s));
+
+			//Console.WriteLine("Press any key...");
 		}
 	}
 }
